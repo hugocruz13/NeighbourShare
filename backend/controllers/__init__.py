0372b8ca620d675f13668392559591e548842cb9
GustaM11 <a27962@alunos.ipca.pt>
2025-03-29 13:21:07 +0000

    Add notificacao_controller to backend routers
    This update includes the notificacao_controller in the list of backend routers, enabling its integration within the routing system. Additionally, fixed a missing period in the import statement for recurso_comum_controller.

--- backend/controllers/__init__.py
+++ backend/controllers/__init__.py
@@ -1,5 +1,6 @@
 from .recurso_controller import router as recurso_controller
 from .reserva_controller import router as reserva_controller
-from.recurso_comum_controller import router as recurso_comum_controller
+from .recurso_comum_controller import router as recurso_comum_controller
+from .notificacao_controller import router as notificacao_controller
 
-routers = [recurso_controller, reserva_controller, recurso_comum_controller]
+routers = [recurso_controller, reserva_controller, recurso_comum_controller, notificacao_controller]
