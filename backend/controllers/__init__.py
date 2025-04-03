521b81d77c4f0f55c466cb46a43777471c0df2ac
GustaM11 <a27962@alunos.ipca.pt>
2025-04-03 19:12:27 +0100

    Add notification management feature with schemas and endpoints
    Introduced schemas, services, and endpoints to manage notifications. Added functionality to create notifications for specific users, admins, or all users, and to list user notifications. Updated repository and controller imports to integrate the new notification functionality.

--- backend/controllers/__init__.py
+++ backend/controllers/__init__.py
@@ -3,5 +3,6 @@ from .reserva_controller import router as reserva_controller
 from .recurso_comum_controller import router as recurso_comum_controller
 from .orcamento_controller import router as orcamento_controller
 from .auth_controller import router as auth_controller
+from .notificacoes_controller import router as notificacoes_controller
 
-routers = [recurso_controller, reserva_controller, recurso_comum_controller, orcamento_controller, auth_controller]
+routers = [recurso_controller, reserva_controller, recurso_comum_controller, orcamento_controller, auth_controller, notificacoes_controller]
--- backend/db/repository/notificacao_repo.py
+++ backend/db/repository/notificacao_repo.py
@@ -5,42 +5,101 @@ from datetime import datetime
 from fastapi import HTTPException
 from sqlalchemy.exc import SQLAlchemyError
 
-async def cria_notificacao_db(db: Session, notificacao: Notificacao, user_id: int = None):
+# Verifica se o tipo de processo (ID) existe
+async def verifica_tipo_processo(db:Session,tipo_processo_id: int = None):
+    tipo_processo = db.query(TipoProcesso).filter(TipoProcesso.TipoProcID == tipo_processo_id).first()
+    if not tipo_processo:
+        raise HTTPException(status_code=404, detail="Tipo de processo não existe")
+    return True
 
+#Cria uma notificação com destino somente um utilizador
+async def cria_notificacao_individual_db(db: Session, notificacao: Notificacao, user_id: int = None):
     try:
-        tipo_processo = db.query(TipoProcesso).filter(TipoProcesso.TipoProcID == notificacao.TipoProcID).first()
-        if not tipo_processo:
-            raise HTTPException(status_code=404, detail="Tipo de processo não existe")
+        if verifica_tipo_processo(db, notificacao.TipoProcID):
 
-        nova_notificao = Notificacao(
-            Mensagem=notificacao.Mensagem,
-            DataHora=datetime.now(),
-            TipoProcID=notificacao.TipoProcID,
-            ProcessoID=notificacao.ProcessoID,
-            Estado=False
-        )
+            nova_notificao = Notificacao(
+                Mensagem=notificacao.Mensagem,
+                DataHora=datetime.now(),
+                TipoProcID=notificacao.TipoProcID,
+                ProcessoID=notificacao.ProcessoID,
+                Estado=False
+            )
 
-        db.add(nova_notificao)
-        db.commit()
-        db.refresh(nova_notificao)
+            db.add(nova_notificao)
+            db.commit()
+            db.refresh(nova_notificao)
 
-        utilizadores_ids = []
+            db.add(NotificacaoUser(UtilizadorID=user_id, NotificacaoID=nova_notificao.NotificacaoID))
+            db.commit()
 
-        if tipo_processo.DescTipoProcesso == 'Individual' and user_id:
-            utilizador = db.query(Utilizador).filter(Utilizador.UtilizadorID == user_id).first()
-            if utilizador:
-                utilizadores_ids = [utilizador.UtilizadorID]
-        elif tipo_processo.DescTipoProcesso == 'Todos':
-            utilizadores_ids = [u[0] for u in db.query(Utilizador.UtilizadorID).all()]
-        elif tipo_processo.DescTipoProcesso == 'Gestores':
-            utilizadores_ids = [u[0] for u in db.query(Utilizador.UtilizadorID).filter(TipoUtilizador.DescTU == 'Gestor').all()]
-
-        for utilizadorID in utilizadores_ids:
-            nova_relacao = NotificacaoUser(NotificacaoID=nova_notificao.NotificacaoID, UtilizadorID=utilizadorID)
-            db.add(nova_relacao)
-            await send_notification(utilizadorID, nova_notificao.Mensagem)
-        db.commit()
-        return True, {'Inserção e envio de notificação realizada com sucesso'}
+            return True, {'Inserção de nova notifcação realizada com sucesso!'}
     except SQLAlchemyError as e:
         db.rollback()
         return False ,{'details': str(e)}
+
+# Cria notificação para somente os gestores/admins
+async def cria_notificacao_admin_db(db: Session, notificacao: Notificacao):
+
+    try:
+        if verifica_tipo_processo(db, notificacao.TipoProcID):
+
+            nova_notificao = Notificacao(
+                Mensagem=notificacao.Mensagem,
+                DataHora=datetime.now(),
+                TipoProcID=notificacao.TipoProcID,
+                ProcessoID=notificacao.ProcessoID,
+                Estado=False
+            )
+            db.add(nova_notificao)
+            db.commit()
+            db.refresh(nova_notificao)
+
+            admins = db.query(Utilizador).filter(TipoUtilizador.DescTU == 'admin')
+
+            for admin in admins:
+                db.add(NotificacaoUser(UtilizadorID=admin.UtilizadorID, NotificacaoID=nova_notificao.NotificacaoID))
+                db.commit()
+            return True, {'Inserção de notifcações para os admins realizada com sucesso!'}
+    except SQLAlchemyError as e:
+        db.rollback()
+        return False ,{'details': str(e)}
+
+# Cria notificação para todos os utilizadores
+async def cria_notificacao_todos_utilizadores_db(db:Session, notificao:Notificacao):
+    try:
+        if verifica_tipo_processo(db, notificao.TipoProcID):
+            nova_notificao = Notificacao(
+                Mensagem=notificao.Mensagem,
+                DataHora=datetime.now(),
+                TipoProcID=notificao.TipoProcID,
+                ProcessoID=notificao.ProcessoID,
+                Estado=False
+            )
+            db.add(nova_notificao)
+            db.commit()
+            db.refresh(nova_notificao)
+
+            utilizadores = db.query(Utilizador).all()
+
+            for utilizador in utilizadores:
+                db.add(NotificacaoUser(UtilizadorID=utilizador.UtilizadorID, NotificacaoID=nova_notificao.NotificacaoID))
+                db.commit()
+
+            return True, {'Inserção de notifcações para todos os utilizadores realizada com sucesso!'}
+    except SQLAlchemyError as e:
+        db.rollback()
+        return False ,{'details': str(e)}
+
+# Lista as notificações de um utilizador por ordem descrescente de data
+async def listar_notificacoes_db(db: Session, user_id: int = None):
+    try:
+        lista_notificacoes = (
+            db.query(Notificacao)
+            .filter(NotificacaoUser.UtilizadorID == user_id)
+            .join(NotificacaoUser)
+            .order_by(Notificacao.DataHora.desc())
+        )
+        return lista_notificacoes
+    except SQLAlchemyError as e:
+        raise SQLAlchemyError(str(e))
+
