98cd74b9ca9502009ab8d1aabda2e6741b8479ca
mtcigor <a27977@alunos.ipca.pt>
2025-04-14 19:22:17 +0100

    Add endpoints and services for managing maintenance states
    Introduced endpoints and services to list and update maintenance and maintenance request states. Updated repository methods to handle state-related operations, ensuring seamless interaction with the database for these functionalities.

--- backend/controllers/recurso_comum_controller.py
+++ backend/controllers/recurso_comum_controller.py
@@ -121,3 +121,24 @@ async def listar_pedidos_manutencao_finalizados(
     """
 
     return await listar_pedidos_manutencao_finalizados_service(db)
+
+@router.get("/pedidodsmanutencao/estados")
+async def listar_tipos_pedido_manutencao(db:Session = Depends(get_db), token: UserJWT=Depends(role_required(["admin", "residente", "gestor"]))):
+    return await obter_all_tipo_estado_pedido_manutencao(db)
+
+@router.put("/pedidosmanutencao/{pedido_id}/estado")
+async def atualizar_estado_pedido(pedido_id: int, estado_data: EstadoUpdate, db: Session = Depends(get_db)):
+    try:
+        obter = await obter_pedido_manutencao(db, pedido_id)
+        if obter is None:
+            raise HTTPException(status_code=404, detail="Pedido de manutenção com o seguinte ID não existe: {pedido_id}")
+        out = await alterar_tipo_estado_pedido_manutencao(db, pedido_id, estado_data.novo_estado_id)
+        if out is False:
+            return False, "Erro ao alterar o tipo de estado do pedido de manutenção com o ID {pedido_id}"
+        if out is True:
+            return True, "Tipo de estado alterado com sucesso"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except HTTPException as es:
+        raise es
+
