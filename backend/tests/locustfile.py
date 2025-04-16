cd7ccb16eb5dea8c37d2a2bcca3066b2eece8ead
GustaM11 <a27962@alunos.ipca.pt>
2025-04-16 12:28:32 +0100

    Add access token handling and update resource request
    Save the access token from login response cookies and use it for authenticated requests to protected resources. Replaced the placeholder function with a functional implementation for accessing personal resources.

--- backend/tests/locustfile.py
+++ backend/tests/locustfile.py
@@ -13,6 +13,7 @@ class TestUser(HttpUser):
         if response.status_code == 200:
             print("Autenticado com sucesso")
 <<<<<<< HEAD
+<<<<<<< HEAD
             self.access_token_cookie = response.cookies.get("access_token")
         else:
             print(f"Erro ao autenticar: {response.status_code} - {response.text}")
@@ -23,11 +24,20 @@ class TestUser(HttpUser):
         # Usa os cookies salvos no login
         self.client.get("/api/recursos/pessoais",  cookies={"access_token": self.access_token_cookie})
 =======
+=======
+            self.access_token_cookie = response.cookies.get("access_token")
+>>>>>>> 6620c9f (Add access token handling and update resource request)
         else:
             print(f"Erro ao autenticar: {response.status_code} - {response.text}")
+            self.access_token_cookie = None
 
-    #TODO Colocar as requisições aos recursos (por exemplo) a funcionar
     @task
+<<<<<<< HEAD
     def testar_endpoint_recursos(self):
         self.client.get("/api/recursos/pessoais")
 >>>>>>> 747f15b (Add initial Locust test script for performance testing)
+=======
+    def acessar_recurso_protegido(self):
+        # Usa os cookies salvos no login
+        self.client.get("/api/recursos/pessoais",  cookies={"access_token": self.access_token_cookie})
+>>>>>>> 6620c9f (Add access token handling and update resource request)
