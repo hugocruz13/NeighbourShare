7999925c3c7f7574a2570ae153e7ca51cd584f57
GustaM11 <a27962@alunos.ipca.pt>
2025-04-22 20:29:52 +0100

    Atualização de validações e restrições nos dados passados pelos schemas

--- backend/schemas/notificacao_schema.py
+++ backend/schemas/notificacao_schema.py
@@ -1,26 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, conint
 import datetime
 
 class TipoProcessoSchema(BaseModel):
-    TipoProcessoID: int
-    DescTipoProcesso: str
+    TipoProcessoID: conint(gt=0)
+    DescTipoProcesso: constr(min_length=3, max_length=100)
 
     class Config:
         from_attributes = True
 
 class NotificacaoUtilizadorSchema(BaseModel):
-    NotificacaoID: int
-    UtilizadorID: int
+    NotificacaoID: conint(gt=0)
+    UtilizadorID: conint(gt=0)
 
     class Config:
         from_attributes = True
 
 class NotificacaoSchema(BaseModel):
-    NotificacaoID: int
-    Mensagem: str
-    DataHora: datetime.date
-    ProcessoID: int
-    Estado: int
+    NotificacaoID: conint(gt=0)
+    Mensagem: constr(min_length=5, max_length=500)
+    DataHora: datetime.datetime
+    ProcessoID: conint(gt=0)
+    Estado: conint(ge=0, le=1)
     TipoProcesso_: TipoProcessoSchema
     Utilizador_: NotificacaoUtilizadorSchema
 
