2350280d10241b7de14c9ee2a017c083cdee81c5
mtcigor <a27977@alunos.ipca.pt>
2025-04-24 16:48:30 +0100

    Whitelist de origens para o CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from controllers import *
 from utils.tokens_record import clean_expired_tokens
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+allow_origins=[
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 clean_expired_tokens()
 
