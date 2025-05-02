f995b7fc052f62af1caf99f0f0a2ed29bf1656ef
mtcigor <a27977@alunos.ipca.pt>
2025-05-02 12:20:56 +0100

    Fix scheduler

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from controllers import *
 from utils.tokens_record import clean_expired_tokens
 from fastapi.middleware.cors import CORSMiddleware
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from services.votacao_service import check_votacoes_expiradas
+from services.recurso_service import checkar_estado_recurso
 
+scheduler = AsyncIOScheduler()
 app = FastAPI()
 
 allow_origins=[
@@ -10,6 +14,14 @@ allow_origins=[
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
+
+
+@app.on_event('startup')
+async def start_scheduler():
+    scheduler.add_job(check_votacoes_expiradas, "interval", hours=24)
+    scheduler.add_job(checkar_estado_recurso, 'cron', hour=0)
+    scheduler.start()
+
 
 app.add_middleware(
     CORSMiddleware,
