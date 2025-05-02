ef842697e0532f53d8ea197893412449e6471e30
hugocruz13 <hugocruz20032003@hotmail.com>
2025-05-02 20:19:35 +0100

    Melhorar tratamento de erros recursos

--- backend/db/repository/recurso_repo.py
+++ backend/db/repository/recurso_repo.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy import update
 from sqlalchemy.orm import joinedload
 from db import session
@@ -15,7 +16,8 @@ def get_disponibilidade_id_db(disponibilidade:str, db:session):
             else:
                 raise Exception("Nenhuma disponibilidade encontrada com o nome informado")
     except SQLAlchemyError as e:
-        raise SQLAlchemyError(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
 
 def get_categoria_id_db(categoria:str, db:session):
     try:
@@ -25,7 +27,8 @@ def get_categoria_id_db(categoria:str, db:session):
         else:
             raise Exception("Nenhuma categoria encontrada com o nome informado")
     except SQLAlchemyError as e:
-        raise SQLAlchemyError(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
 
 async def inserir_recurso_db(db:session, recurso:Recurso):
     try:
@@ -45,16 +48,16 @@ async def inserir_recurso_db(db:session, recurso:Recurso):
         return novo_recurso.RecursoID, {'Inserção do recurso realizada com sucesso!'}
     except SQLAlchemyError as e:
         db.rollback()
-        return False, {'details': str(e)}
+        raise HTTPException(status_code=400, detail=str(e))
 
 async def update_path(db: session, id: int, path: str):
     try:
         db.query(Recurso).filter(Recurso.RecursoID == id).update({Recurso.Path: path})
         db.commit()
         return {"message": "Caminho da imagem atualizado com sucesso."}
-    except Exception as e:
+    except SQLAlchemyError as e:
         db.rollback()
-        raise RuntimeError(f"Erro ao guardar o caminho da imagem: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
 
 #Lista todos os recursos registados no sistema
 async def listar_recursos_db(db:session):
@@ -70,7 +73,8 @@ async def listar_recursos_db(db:session):
         )
         return recursos
     except SQLAlchemyError as e:
-        raise SQLAlchemyError(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
 
 #Lista as informações referentes a um recurso registado no sistema
 async def listar_recurso_db(db:session, recurso_id:int):
@@ -78,7 +82,8 @@ async def listar_recurso_db(db:session, recurso_id:int):
         recurso = db.query(Recurso).filter(Recurso.RecursoID == recurso_id).first()
         return recurso
     except SQLAlchemyError as e:
-        raise SQLAlchemyError(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
 
 #Lista os recursos pertencentes a um utilizador (utilizador_id)
 async def listar_recursos_utilizador_db(db:session, utilizador_id:int):
@@ -95,31 +100,35 @@ async def listar_recursos_utilizador_db(db:session, utilizador_id:int):
         )
         return recursos
     except SQLAlchemyError as e:
-        raise SQLAlchemyError(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
 
 #Função para atualizar o estado de todos os recursos tendo em conta se está reservado ou não
 async def atualizar_disponibilidade_recurso_db(db:session):
-    hoje = date.today()
+    try:
+        hoje = date.today()
 
-    recursos = db.query(Recurso).all()
+        recursos = db.query(Recurso).all()
 
-    for recurso in recursos:
-        reserva_ativa = (
-            db.query(Reserva)
-            .join(PedidoReserva, PedidoReserva.RecursoID == Recurso.RecursoID)
-            .filter(
-                PedidoReserva.RecursoID == Recurso.RecursoID,
-                PedidoReserva.DataInicio <= hoje,
-                PedidoReserva.DataFim >= hoje,
-                PedidoReserva.EstadoPedidoReserva_.DescEstadoPedidoReserva == PedidoReservaEstadosSchema.APROVADO
+        for recurso in recursos:
+            reserva_ativa = (
+                db.query(Reserva)
+                .join(PedidoReserva, PedidoReserva.RecursoID == Recurso.RecursoID)
+                .filter(
+                    PedidoReserva.RecursoID == Recurso.RecursoID,
+                    PedidoReserva.DataInicio <= hoje,
+                    PedidoReserva.DataFim >= hoje,
+                    PedidoReserva.EstadoPedidoReserva_.DescEstadoPedidoReserva == PedidoReservaEstadosSchema.APROVADO
+                )
+                .first()
             )
-            .first()
-        )
 
-        recurso.DispID = DisponibilidadeEstadosSchema.INDISPONIVEL if reserva_ativa else DisponibilidadeEstadosSchema.DISPONIVEL
+            recurso.DispID = DisponibilidadeEstadosSchema.INDISPONIVEL if reserva_ativa else DisponibilidadeEstadosSchema.DISPONIVEL
 
-    db.commit()
-
+        db.commit()
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
 #Função que muda a disponibilidade de um recurso para indisponivel
 async def muda_recurso_para_indisponivel(db:session, recurso_id:int):
     try:
@@ -127,4 +136,5 @@ async def muda_recurso_para_indisponivel(db:session, recurso_id:int):
         recurso.DispID = DisponibilidadeEstadosSchema.INDISPONIVEL
         return True
     except SQLAlchemyError as e:
-        raise SQLAlchemyError(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(e))
