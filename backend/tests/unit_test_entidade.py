e3db2247bf622534e5841275aa6796330dbcedda
hugocruz13 <hugocruz20032003@hotmail.com>
2025-05-03 17:11:45 +0100

    Automatização teste entidade externa

--- backend/tests/unit_test_entidade.py
+++ backend/tests/unit_test_entidade.py
@@ -23,6 +23,8 @@ async def test_registar_entidade(db_session):
 
     #Assert
     assert registo[0]==True
+    assert registo[1]=={'Nova entidade inserida com sucesso.'}
+
 
 async def test_registar_entidade_schema(db_session):
     # Arrange & Assert
@@ -74,7 +76,7 @@ async def test_update_entidade_service(db_session):
     entidade =await inserir_entidade_testes(db_session, EntidadeExterna(Especialidade="Elevadores", Contacto=253787945,Email="teste@teste.com",Nome="Elevadores", Nif=123456789))
     entidade_teste = EntidadeUpdateSchema(EntidadeID=entidade.EntidadeID,Especialidade="Carros",Contacto=253945787,Email="carros@teste.com",Nome="Carrps",Nif=678912345)
     #Act
-    test = await update_entidade_service(entidade, db_session)
+    test = await update_entidade_service(entidade_teste, db_session)
 
     #Assert
     assert test[0]==True
