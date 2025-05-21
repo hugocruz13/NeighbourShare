697119178077cd5a6f427d1741d6921cfd3061f0
hugocruz13 <hugocruz20032003@hotmail.com>
2025-05-21 16:40:16 +0100

    Apagar coisas que não estão a ser utilizadas

--- frontend/src/pages/Admin.js
+++
@@ -1,8 +0,0 @@
-function Admin() {
-    return (
-        <h1>Admin</h1>
-    );
-  }
-  
-  export default Admin;
-  
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.js";
-import { InputLogin, InputPassword } from "../components/Inputs.js";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "../styles/Login.css";
