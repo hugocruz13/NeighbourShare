c0339e3b89db308b0ac8630ebb0913f5e1014f9d
mtcigor <a27977@alunos.ipca.pt>
2025-05-22 17:22:51 +0100

    Bug fix

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -6,9 +6,10 @@ import Button from "../components/Button.jsx";
 import { useState, useEffect } from "react";
 
 function Residente() {
-  const [ user, setUser ] = useState(null);
+  const { user } = useAuth();
   const navigate = useNavigate();
   const [greeting, setGreeting] = useState("");
+  const [usr, setUsr] = useState(null);
 
   useEffect(() => {
     const hour = new Date().getHours();
@@ -32,7 +33,7 @@ function Residente() {
 
         const data = await response.json();
         console.log(data)
-        setUser(data);
+        setUsr(data);
       } catch (error) {
         console.error('Erro:', error);
       }
@@ -47,75 +48,9 @@ function Residente() {
   return (
     <div className="page-content">
       <Navbar2 />
-      <div className="home-containerMenu">
-        <p className="menu-title">Menu Principal</p>
-        <div className="card-grid">
-          <div className="card">
-            <img src="img/notificacoes_back.jpeg" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/notificacoes">Notificações</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/pedidos_reserva.jpeg" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/listaPedidosReserva">Pedidos de Reserva</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/reservas.jpg" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/listaReserva">Reservas</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/meus_recursos.png" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/meusRecursos">Meus Recursos</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/pedido_manutencao.jpeg" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/realizarPedidoManutencao">Realizar Pedido Manutenção</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/pedido_novo_rec.png" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/realizarPedidoNovoRecurso">Realizar Pedido Novo Recurso</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/recursos_disp.jpeg" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/recursosDisponiveis">Recursos Disponíveis</Link><br />
-          </div>
-          <div className="card">
-            <img src="img/votacoes.jpeg" alt="Descrição" className="card-img" />
-            <Link className="card-text" to="/votacoes">Votações</Link><br />
-          </div>
-          
-
-          {/* Apenas Gestores */}
-          {user?.role === "gestor" && (
-            <>
-              <div className="card">
-                <img src="img/manutencao.jpg" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/manutencao">Manutenções</Link><br />
-              </div>
-              <div className="card">
-                <img src="img/entidades.jpg" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/entidadeExterna">Entidades Externas</Link><br />
-              </div>
-              <div className="card">
-                <img src="img/orcamentos.png" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/orcamentos">Orçamentos</Link><br />
-              </div>
-              <div className="card">
-                <img src="img/pedidos_manutencoes.jpeg" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/pedidosManutencao">Pedidos Manutenção</Link><br />
-              </div>
-              <div className="card">
-                <img src="img/pedidos_novos_rec.jpg" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/pedidosNovosRecursos">Pedidos Novos Recursos</Link><br />
-              </div>
-              <div className="card">
-                <img src="img/rec_comum.jpg" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/recursosComuns">Recursos Comuns</Link><br />
-              </div>
-            </>
-          )}
       <div className="dashboard-container">
         <div className="dashboard-header">
-          <h1>{greeting}, <span className="user-name">{user?.nome || "Vizinho"}</span></h1>
+          <h1>{greeting}, <span className="user-name">{usr?.nome || "Vizinho"}</span></h1>
           <p className="welcome-text">Bem-vindo ao seu painel de gestão NeighbourShare</p>
         </div>
         
@@ -201,17 +136,6 @@ function Residente() {
           </div>
         )}
 
-          {/* Apenas Admins */}
-          {user?.role === "admin" && (
-            <>
-              <div className="card">
-                <img src="img/registar.jpg" alt="Descrição" className="card-img" />
-                <Link className="card-text" to="/registar">Registar</Link><br />
-              </div>
-            </>
-          )}
-
-        </div>
         {/* Seção para Admins */}
         {user?.role === "admin" && (
           <div className="management-section">
@@ -227,7 +151,6 @@ function Residente() {
 
       </div>
     </div>
-
   );
 }
 
